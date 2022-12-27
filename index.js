6d29b39e1a445a8606ada81ada66738fc9777b0a
Sinan997 <sinanoz3437@gmail.com>
2022-12-27 14:17:18 +0300

    start script added

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get("/",(req,res)=>{
 })
 
 app.listen(PORT,()=>{
-    console.log("server is listening at http://localhost:",PORT);
+    console.log("server is listening at http://localhost:"+PORT);
 })
